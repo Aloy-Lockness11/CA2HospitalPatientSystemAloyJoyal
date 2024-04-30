8a737ea56edb470c33fa17006b2ad19aea9e3c7c
Aloysius <aloysiuspacheco2003@gmail.com>
2024-04-30 16:25:18 +0100

    Setters with exceptions added to Appointments

--- src/main/java/HospitalObjects/Appointment.java
+++ src/main/java/HospitalObjects/Appointment.java
@@ -49,4 +49,54 @@ public class Appointment {
     public String getDoctorFullName() {
         return doctorFullName;
     }
+    //setters
+    public void setPatientFirstName(String patientFirstName) {
+        if (patientFirstName == null || patientFirstName.isEmpty()) {
+            throw new IllegalArgumentException("Patient first name cannot be empty");
+        }
+        this.patientFirstName = patientFirstName;
+    }
+    public void setPatientLastName(String patientLastName) {
+        if (patientLastName == null || patientLastName.isEmpty()) {
+            throw new IllegalArgumentException("Patient last name cannot be empty");
+        }
+        this.patientLastName = patientLastName;
+    }
+    public void setDoB(LocalDate DoB) {
+        if (DoB == null) {
+            throw new IllegalArgumentException("Date of birth cannot be empty");
+        }else if (DoB.isAfter(LocalDate.now())) {
+            throw new IllegalArgumentException("Date of birth cannot be in the future");
+        }
+        this.DoB = DoB;
+    }
+    public void setIssue(String issue) {
+        if (issue == null || issue.isEmpty()) {
+            throw new IllegalArgumentException("Issue cannot be empty");
+        }
+        if (issue.length() > 500) {
+            throw new IllegalArgumentException("Issue cannot be more than 500 characters");
+
+        }
+        this.issue = issue;
+    }
+    public void setDate(LocalDate date) {
+        if (date == null) {
+            throw new IllegalArgumentException("Date cannot be empty");
+        }
+        this.date = date;
+    }
+    public void setTriage(int triage) {
+        if (triage < 1 || triage > 5) {
+            throw new IllegalArgumentException("Triage must be between 1 and 5");
+        }
+        this.triage = triage;
+    }
+    public void setDoctorFullName(String doctorFullName) {
+        if (doctorFullName == null || doctorFullName.isEmpty()) {
+            throw new IllegalArgumentException("Doctor full name cannot be empty");
+        }
+        this.doctorFullName = doctorFullName;
+    }
+
 }
