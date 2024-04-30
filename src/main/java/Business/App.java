0a27ae817726b298e1c87a807c5780ec009a8e7b
Aloysius <aloysiuspacheco2003@gmail.com>
2024-04-30 14:24:27 +0100

    Main Project Structure

---
+++ src/main/java/Business/App.java
@@ -0,0 +1,8 @@
+package Business;
+/*
+* Applications for the Hospital to roster doctors
+* @author Aloy And Joyal
+* @version 1.0
+* */
+public class App {
+}
---
+++ src/main/java/HospitalObjects/Appointment.java
@@ -0,0 +1,4 @@
+package HospitalObjects;
+
+public class Appointment {
+}
