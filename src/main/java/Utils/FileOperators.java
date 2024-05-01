f29dce328e50e33b87a319630dad5cc3dabd53c1
Aloysius <aloysiuspacheco2003@gmail.com>
2024-05-01 05:28:19 +0100

    File Operators added

--- src/main/java/Utils/FileOperators.java
+++ src/main/java/Utils/FileOperators.java
@@ -1,23 +1,45 @@
 package Utils;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 
-import HospitalObjects.Appointment;
+import HospitalObjects.Patient;
 
 public class FileOperators {
-    public static void saveAppointmentsToBinFile(Appointment appointments, String filename) {
+    /**
+     * Saves the patient to a binary file
+     *
+     * @param patient  the patient to save
+     * @param filename the name of the file to save the patient to
+     */
+    public static void savePatientsToBinFile(ArrayList<Patient> patient, String filename) {
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filename))) {
-            oos.writeObject(appointments);
-            System.out.println("Appointments saved to file: " + filename);
+            oos.writeObject(patient);
+            System.out.println("Patients saved to file: " + filename);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static void readAppointmentsFromBinFile(String filename) {
-        // TODO  Implement this method
+    /**
+     * Reads the patient from a binary file
+     *
+     * @param filename the name of the file to read the patient from
+     * @return the patient read from the file
+     */
+    public static ArrayList<Patient> readAppointmentsFromBinFile(String filename) {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename))) {
+            ArrayList<Patient> patient = (ArrayList<Patient>) ois.readObject();
+            System.out.println("Patient read from file: " + filename);
+            return patient;
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
